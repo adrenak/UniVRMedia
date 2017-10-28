bdbb00556d9553f4ae08f14b6288210ac286ec94
firexitsoftware <vatsal@firexitsoftware.com>
2017-10-28 20:24:26 +0530

    Initial commit

---
+++ Assets/Uni360Video/Scripts/Context.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Context : MonoBehaviour {
+    [SerializeField]
+    Uni360Video.Options options;
+
+    public void Show360Video () {
+        Uni360Video.Init(options);
+    }
+
+    public void Stop360Video() {
+        Uni360Video.Stop();
+    }
+}
---
+++ Assets/Uni360Video/Scripts/NormalHelper.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Based on ReverseNormals.cs from Joachim Ante here : http://wiki.unity3d.com/index.php/ReverseNormals
+ */
+
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshFilterHelper {
+    public static void InvertNormals(MeshFilter filter) {
+        if (filter != null) {
+            Mesh mesh = filter.mesh;
+
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < normals.Length; i++)
+                normals[i] = -normals[i];
+            mesh.normals = normals;
+
+            for (int m = 0; m < mesh.subMeshCount; m++) {
+                int[] triangles = mesh.GetTriangles(m);
+                for (int i = 0; i < triangles.Length; i += 3) {
+                    int temp = triangles[i + 0];
+                    triangles[i + 0] = triangles[i + 1];
+                    triangles[i + 1] = temp;
+                }
+                mesh.SetTriangles(triangles, m);
+            }
+        }
+    }
+}
